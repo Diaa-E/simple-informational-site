974421d620c7da416ec75db5b1130cc35ce36fcd
Diaa-E <blarb21@yahoo.com>
2024-05-10 07:40:23 +0300

    Create router

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 import http from "node:http";
+import path from "node:path";
+import url from "node:url";
+import fs from "node:fs/promises";
 
 const PORT = process.env.PORT;
 const HOSTNAME = process.env.HOSTNAME;
 
-const server = http.createServer((req, res) => {
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-    res.writeHead(200, "Content-Type", "text/html");
-    res.write("<h1>Hello world</h1>");
+const server = http.createServer( async (req, res) => {
+
+    let status;
+    let filepath;
+
+    if (req.method === "GET")
+    {
+        switch (req.url)
+        {
+            case "/":
+                status = 200;
+                filepath = path.join(__dirname, "public", "index.html");
+                break;
+            
+            case "/about":
+                status = 200;
+                filepath = path.join(__dirname, "public", "about.html");
+                break;
+
+            case "/contact":
+                status = 200;
+                filepath = path.join(__dirname, "public", "contact.html");
+                break;
+
+            default: 
+                status = 404;
+                filepath = path.join(__dirname, "public", "error.html");
+        }
+
+    }
+
+    res.writeHead(status, "Content-Type", "text/html");
+    const page = await fs.readFile(filepath);
+    res.write(page);
     res.end();
 });
 
