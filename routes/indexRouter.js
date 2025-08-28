bf40e91ce821b28d7db3da3a29275d9c9ce7501f
Diaa-E <blarb21@yahoo.com>
2025-08-28 17:13:41 +0300

    Remove users from home page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,25 +1,13 @@
 import { Router } from "express";
 import { __dirname } from "../rootDir.js";
-import path from "node:path";
 import CustomNotFoundError from "../errors/customBotFoundError.js";
 import links from "../utils/links.js";
-
-const PUBLIC_PATH = path.join(__dirname, "public");
-
-const users = [
-    "John",
-    "Bruce",
-    "Mark",
-    "Adam",
-    "Anita",
-    "Joanna"
-]
 
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => {
 
-    res.render("index", { message: "Rendered EJS", links: links, users: users });
+    res.render("index", { message: "Rendered EJS", links: links});
 })
 .get("/about", (req, res) => {
 
