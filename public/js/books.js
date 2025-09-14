934937e22111a7b0b49dbb17bf88679f0bb28407
Diaa-E <blarb21@yahoo.com>
2025-09-14 15:14:43 +0300

    Animate book list items

--- public/js/books.js
+++ public/js/books.js
@@ -4,3 +4,13 @@ const titleElement = document.querySelector(".page-title>.visible-text");
 const titleText = document.querySelector(".page-title>.hidden-text").textContent.trim();
 
 animateTypewriter(titleText, titleElement, 100);
+
+const listItems = document.querySelectorAll("li.entry-list-item");
+
+for (let i = 0; i < listItems.length; i++)
+{
+    setTimeout(() => {
+
+        listItems[i].classList.add("visible");
+    }, 50 * i);
+}
